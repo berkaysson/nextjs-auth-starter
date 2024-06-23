413902526c3075a48027db625f17bc4fd0d91be5
berkaysson <berkaysonel85@gmail.com>
2024-06-23 21:22:56 +0300

    add settings page

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,23 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+
+const SettingsPage = async () => {
+  const session = await auth();
+  return (
+    <div>
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <Button variant={"destructive"} size={"default"}>
+          Sign Out
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default SettingsPage;
