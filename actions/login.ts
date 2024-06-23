2b0dfc2306d0aa90b0ff031b2f8a2c8145799340
berkaysson <berkaysonel85@gmail.com>
2024-06-23 21:23:10 +0300

    update login server action

--- actions/login.ts
+++ actions/login.ts
@@ -1,14 +1,36 @@
 "use server";
 
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema } from "@/schemas";
+import { AuthError } from "next-auth";
 import { z } from "zod";
 
-export const login = (data: z.infer<typeof LoginSchema>) => {
+export const login = async (data: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(data);
 
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password } = validatedFields.data;
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      console.log("🚀 ~ file: login.ts:25 ~ login ~ error:", error)
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials!" };
+        default:
+          return { error: "Authentication error" };
+      }
+    }
+
+    throw error;
+  }
 };
